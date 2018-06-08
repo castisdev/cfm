6edf824fe65db933f02e781816f6043c2007a9f7
Yang Heesung <fshilver@gmail.com>
2018-06-08 11:20:55 +0900

    DiskUsage struct 필요없는 변수 삭제
    - FileSystem
    - MountPoint

--- remover/remover_test.go
+++ remover/remover_test.go
@@ -19,8 +19,8 @@ func Test_collectRemoteDiskUsage(t *testing.T) {
 
 	// setup dummy http server
 	d1 := common.DiskUsage{
-		FileSystem: "ostype1", TotalSize: 1000, UsedSize: 500,
-		FreeSize: 500, UsedPercent: 50, MountPoint: "/data1",
+		TotalSize: 1000, UsedSize: 500,
+		FreeSize: 500, UsedPercent: 50,
 	}
 	h1 := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
@@ -31,8 +31,8 @@ func Test_collectRemoteDiskUsage(t *testing.T) {
 	})
 
 	d2 := common.DiskUsage{
-		FileSystem: "ostype2", TotalSize: 2000, UsedSize: 1000,
-		FreeSize: 1000, UsedPercent: 50, MountPoint: "/data2",
+		TotalSize: 2000, UsedSize: 1000,
+		FreeSize: 1000, UsedPercent: 50,
 	}
 	h2 := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
@@ -43,8 +43,8 @@ func Test_collectRemoteDiskUsage(t *testing.T) {
 	})
 
 	d3 := common.DiskUsage{
-		FileSystem: "ostype3", TotalSize: 3000, UsedSize: 1500,
-		FreeSize: 1500, UsedPercent: 50, MountPoint: "/data3",
+		TotalSize: 3000, UsedSize: 1500,
+		FreeSize: 1500, UsedPercent: 50,
 	}
 	h3 := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
@@ -83,26 +83,20 @@ func Test_collectRemoteDiskUsage(t *testing.T) {
 	dus := make(map[string]*common.DiskUsage)
 	collectRemoteDiskUsage(hosts, dus)
 
-	assert.Equal(t, d1.FileSystem, dus[vs1].FileSystem)
 	assert.Equal(t, d1.TotalSize, dus[vs1].TotalSize)
 	assert.Equal(t, d1.UsedSize, dus[vs1].UsedSize)
 	assert.Equal(t, d1.FreeSize, dus[vs1].FreeSize)
 	assert.Equal(t, d1.UsedPercent, dus[vs1].UsedPercent)
-	assert.Equal(t, d1.MountPoint, dus[vs1].MountPoint)
 
-	assert.Equal(t, d2.FileSystem, dus[vs2].FileSystem)
 	assert.Equal(t, d2.TotalSize, dus[vs2].TotalSize)
 	assert.Equal(t, d2.UsedSize, dus[vs2].UsedSize)
 	assert.Equal(t, d2.FreeSize, dus[vs2].FreeSize)
 	assert.Equal(t, d2.UsedPercent, dus[vs2].UsedPercent)
-	assert.Equal(t, d2.MountPoint, dus[vs2].MountPoint)
 
-	assert.Equal(t, d3.FileSystem, dus[vs3].FileSystem)
 	assert.Equal(t, d3.TotalSize, dus[vs3].TotalSize)
 	assert.Equal(t, d3.UsedSize, dus[vs3].UsedSize)
 	assert.Equal(t, d3.FreeSize, dus[vs3].FreeSize)
 	assert.Equal(t, d3.UsedPercent, dus[vs3].UsedPercent)
-	assert.Equal(t, d3.MountPoint, dus[vs3].MountPoint)
 }
 
 func TestSetDiskUsageLimitPercent(t *testing.T) {
